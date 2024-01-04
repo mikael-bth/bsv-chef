3afc76a3257c4dc3009137e71f2e7c016a64cca8
mikael-bth <mikmen1109@gmail.com>
2024-01-04 15:23:10 +0100

    Rest of the test added

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,54 +1,67 @@
 import pytest
+import unittest.mock as mock
 
 from src.controllers.recipecontroller import RecipeController
-from src.util.dao import DAO
 
-# Creating DAO with new collection for testing
-testDAO = DAO("test")
-testDAO.create({
-    "name": "Salt",
+items = [{"name": "Salt",
     "quantity": 275,
-    "unit": "gram"})
-testDAO.create({
+    "unit": "gram"}, {
     "name": "Sugar",
     "quantity": 300,
-    "unit": "gram"})
-testDAO.create({
+    "unit": "gram"}, {
     "name": "Flour",
     "quantity": 450,
-    "unit": "gram"})
+    "unit": "gram"}]
 
-controller = RecipeController(testDAO)
+@pytest.fixture
+def sut():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = items
+    mockedsut = RecipeController(mockedDAO)
+    return mockedsut
 
 # add your test case implementation here
 @pytest.mark.unit
-def test_get_all():
+def test_get_all(sut):
     """
     Test that get_all doesnt raise an exception
     """
     try:
-        controller.get_available_items()
+        sut.get_available_items()
     except Exception:
         assert False
 
 @pytest.mark.unit
-def test_minimum_quantity_0():
+def test_minimum_quantity_0(sut):
     """
     Test that get_available_items returns the right item with minimum_quantity 0
     """
-    items = controller.get_available_items(0)
+    result = sut.get_available_items(0)
 
-    assert items == {{
-    "name": "Salt",
-    "quantity": 275,
-    "unit": "gram"}, {
-    "name": "Sugar",
-    "quantity": 300,
-    "unit": "gram"}, {
-    "name": "Flour",
-    "quantity": 450,
-    "unit": "gram"}}
+    assert result == {"Salt": 275, "Sugar": 300, "Flour": 450}
 
+@pytest.mark.unit
+def test_minimum_quantity_280(sut):
+    """
+    Test that get_available_items returns the right item with minimum_quantity 300
+    """
+    result = sut.get_available_items(280)
 
-# Drops the collection when tests are done
-testDAO.drop()
+    assert result == {"Sugar": 300, "Flour": 450}
+@pytest.mark.unit
+def test_minimum_quantity_400(sut):
+    """
+    Test that get_available_items returns the right item with minimum_quantity 300
+    """
+    result = sut.get_available_items(400)
+
+    assert result == {"Flour": 450}
+
+@pytest.mark.unit
+def test_minimum_quantity_400(sut):
+    """
+    Test that get_available_items returns the right item with minimum_quantity 300
+    """
+    result = sut.get_available_items(500)
+
+    assert result == {}
