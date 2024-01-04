dcb3315331088aa10cf6c4e273a9ba3a27e77610
mikael-bth <mikmen1109@gmail.com>
2024-01-04 14:49:38 +0100

    New test and test of workflow

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -31,5 +31,24 @@ def test_get_all():
     except Exception:
         assert False
 
+@pytest.mark.unit
+def test_minimum_quantity_0():
+    """
+    Test that get_available_items returns the right item with minimum_quantity 0
+    """
+    items = controller.get_available_items(0)
+
+    assert items == {{
+    "name": "Salt",
+    "quantity": 275,
+    "unit": "gram"}, {
+    "name": "Sugar",
+    "quantity": 300,
+    "unit": "gram"}, {
+    "name": "Flour",
+    "quantity": 450,
+    "unit": "gram"}}
+
+
 # Drops the collection when tests are done
 testDAO.drop()
